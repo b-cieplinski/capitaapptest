39bec3e87a7df91548ed7de94ad14357521dd942
Brandon Cieplinski <71282502+b-cieplinski@users.noreply.github.com>
2022-06-27 11:39:53 +0200

    gh link update

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -39,7 +39,7 @@ function Header(props) {
             <Grid item xs />
             <Grid item>
               <LinkIcon
-                href="/"
+                href="https://github.com/b-cieplinski"
                 variant="body2"
                 sx={{
                   textDecoration: 'none',
